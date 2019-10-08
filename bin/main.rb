f58342b91df2c2e683c1d7917338af13bd30d4d6
Tiago Ferreira <tiferreira12@gmail.com>
2019-10-08 18:38:46 -0300

    initial class definition

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,45 @@
 #!/usr/bin/env ruby 
 
-puts 'Hello, World!'
+class Player
+  attr_accessor :number
+  attr_accessor :name
+
+  def initialize(number, name)
+    @number = number
+    @name = name
+  end
+end
+
+class TicTacToe
+  
+  def initialize(players)
+    @players = []
+    @players[0] = players[0]
+    @players[1] = players[1]
+  end
+  
+end
+
+
+def start_game
+  puts "Welcome to Tic Tac Toe game.\n\n"
+
+  players = []
+  2.times do |i|
+    print "Please tell us player #{i + 1} name: "
+    name = gets.chomp
+    # TODO: validate (min 3 letters)
+    players[i] = Player.new(i, name)
+    puts players[i]
+  end
+
+  game = TicTacToe.new(players)
+  # game.print_board
+
+  # Print the keyboard with numbers from 1 to 9
+  def print_board
+
+  end
+end
+
+start_game
---
+++ bin/temp.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby 
+
+class Player
+  attr_accessor :number
+  attr_accessor :name
+
+  def initialize(number, name)
+    @number = number
+    @name = name
+  end
+end
+
+class TicTacToe
+  @turn = 0
+  @board = (0..8).to_a
+  @players = []
+
+  def initialize(player1, player2)
+    @players[0] = player1
+    @players[1] = player2
+  end
+
+
+  def turn
+    # TODO: validation
+    # do 
+    puts @players[turn] << ' please chose a number: '
+    num = gets.chomp
+    # validate input num from 1-9 not already choose
+    # if num >= 1 && num <=9 && \d\.match(board[num-1])
+    puts 'Number already choosen';
+    # while invalid_input
+
+    turn += 1
+  end
+
+  # 
+  def end_game
+    puts "We have a draw"
+    puts "Congratulation #{players[turn % 2]} you win!"
+
+    puts "Want to play again? [Y/n]"
+    # anything other than n means Y
+    again = gets.chomp
+  end
+
+  # check if the game is over
+  def game_over?
+    # minimum number of turns = 5
+    # if row or colum or diagonal
+    # maximum if turn == 9 
+  end
+
+
+
+end
+
+
+def start_game
+  puts "Welcome to Tic Tac Toe game.\n\n"
+
+  players = []
+  2.times do |i|
+    print "Please tell us player #{i + 1} name: "
+    name = gets.chomp
+    # TODO: validate (min 3 letters)
+    players[i] = Player.new(i, name)
+    puts players[i]
+
+  end
+
+  game = TicTacToe.new(players[0], players[1])
+  # game.print_board
+end
+
+start_game
