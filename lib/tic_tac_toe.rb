d0812ca6af2eb81d00713a81fadb70d1b687b25b
Tiago Ferreira <tiferreira12@gmail.com>
2019-10-14 14:43:31 -0300

    fix brken test

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ class TicTacToe
     @turn = 0
 
     @enum_status = { in_progress: 0, win: 1, draw: 2 } # enum
-    @player_char = %w[X 0]
+    @player_char = %w[X O]
 
     @status = @enum_status[:in_progress]
   end
